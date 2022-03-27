236ec2f13ff6b0ac2bff6977a56ae35754e4acd5
agenjo1iyabo# <maryam.abdulkareem@cyphercrescent.com>
2022-03-27 17:42:26 +0100

    Assignment One for Arrays

---
+++ CCL_ARRAY_ASSIGNMENT_0NE/QUESTION_2B/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace QUESTION_2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            int[,] matrix1 = new int[3, 3];
+            int[,] matrix2 = new int[3, 3];
+            int[,] matrixSubtraction = new int[3, 3];
+
+            Console.Write("\n\nsubtraction of two Matrices :\n");
+            Console.Write("-----------------------------------------\n");
+            Console.Write("Input the size of the square matrix (less than 5): ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            /* Stored values into the array*/
+            Console.Write("Input elements in the first matrix :\n");
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write("element - [{0},{1}] : ", i, j);
+                    matrix1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.Write("Input elements in the second matrix :\n");
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write("element - [{0},{1}] : ", i, j);
+                    matrix2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.Write("\nThe First matrix is :\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < n; j++)
+                    Console.Write("{0}\t", matrix1[i, j]);
+            }
+
+            Console.Write("\nThe Second matrix is :\n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < n; j++)
+                    Console.Write("{0}\t", matrix2[i, j]);
+            }
+            /* calculate the subtraction of the matrix */
+            for (i = 0; i < n; i++)
+                for (j = 0; j < n; j++)
+                    matrixSubtraction[i, j] = matrix1[i, j] - matrix2[i, j];
+            Console.Write("\nThe subtraction of two matrix is : \n");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < n; j++)
+                    Console.Write("{0}\t", matrixSubtraction[i, j]);
+            }
+            Console.Write("\n\n");
+
+
+        }
+    }
+}
---
+++ CCL_ARRAY_ASSIGNMENT_0NE/QUESTION_2D/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace QUESTION_2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[,] matrix1 = new int[3, 3];
+            int[,] matrix2 = new int[3, 3];
+            int det = 0;
+            int det2 = 0;
+
+
+            Console.Write("\n\nCalculate the determinant of a 3 x 3 matrix :\n");
+            Console.Write("-------------------------------------------------\n");
+
+            Console.Write("Input elements in the matrix :\n");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("element - [{0}],[{1}] : ", i, j);
+                    matrix1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.Write("The matrix is :\n");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                    Console.Write("{0}  ", matrix1[i, j]);
+                Console.Write("\n");
+            }
+
+            for (i = 0; i < 3; i++)
+                det = det + (matrix1[0, i] * (matrix1[1, (i + 1) % 3] * matrix1[2, (i + 2) % 3] - matrix1[1, (i + 2) % 3] * matrix1[2, (i + 1) % 3]));
+            Console.WriteLine("The determinat of the first matrix is {0}", det);
+
+            Console.Write("\n\nCalculate the determinant of the second  3 x 3 matrix :\n");
+            Console.Write("-------------------------------------------------\n");
+
+            Console.Write("Input elements in the matrix :\n");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("element - [{0}],[{1}] : ", i, j);
+                    matrix2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.Write("The matrix is :\n");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                    Console.Write("{0}  ", matrix2[i, j]);
+                Console.Write("\n");
+            }
+
+            for (i = 0; i < 3; i++)
+                det2 = det2 + (matrix2[0, i] * (matrix2[1, (i + 1) % 3] * matrix2[2, (i + 2) % 3] - matrix2[1, (i + 2) % 3] * matrix2[2, (i + 1) % 3]));
+
+            Console.WriteLine("The determinat of the second matrix is {0}", det2);
+
+            Console.WriteLine(det > det2 ? "The determinat of the first matrix is bigger than the second matrix" : "The determinat of the second matrix is bigger than the first matrix");
+
+
+        }
+    }
+}
